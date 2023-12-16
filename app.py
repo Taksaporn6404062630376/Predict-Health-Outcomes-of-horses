b24c9ed17f25acc591d4e0716e2876303f40c8f6
Taksaporn6404062630376 <s6404062630376@email.kmutnb.ac.th>
2023-12-16 18:43:35 +0700

    5 commit

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 import streamlit as st
 import pickle
 import pandas as pd
-# import matplotlib.pyplot as plt
-# import plotly.express as px
+#import matplotlib.pyplot as plt
+#import plotly.express as px
 
 model_path = "C:\\Users\\USER\\mlweb\\randomforest.pkl"
 with open(model_path, 'rb') as file:
